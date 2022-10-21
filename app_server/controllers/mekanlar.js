62d973ac4a9bcc96ee99386e914f0ecdffe1a045
kgnkync <115173693+kgnkync@users.noreply.github.com>
2022-10-21 19:33:46 +0000

    guncel 1.1

--- app_server/controllers/mekanlar.js
+++ app_server/controllers/mekanlar.js
@@ -1,23 +1,89 @@
 var express = require('express');
 var router = express.Router();
 
-const anaSayfa = function (req, res,) {
-  res.render('anasayfa', { 'title': 'Ana sayfa' });
+//anasayfa ismi sabit kalması için const yapılır
+
+const anaSayfa = function(req, res, next) {
+  res.render('anasayfa',
+    {
+      "baslik": 'Anasayfa',
+      "sayfaBaslik": {
+        "siteAd": "MekanBul",
+        "slogan": "Civardaki Mekanları Kesfet"
+      },
+      "mekanlar": [
+        {
+          "ad": "Starbucks",
+          "puan": "5",
+          "adres": "Centrum Garden AVM",
+          "imkanlar": ["Kahve", "Çay", "Kek"],
+          "mesafe": "10km"
+        },
+        {
+          "ad": "Antre Kitchen",
+          "puan": "4",
+          "adres": "İyaş Awm",
+          "imkanlar": ["Kahve", "Limonata", "Kek", "Pasta"],
+          "mesafe": "3km"
+        }
+
+      ]
+
+    });
 }
 
-const mekanBilgisi = function (req, res,) {
-  res.render('mekanbilgisi', { 'title': 'Mekan bilgisi' });
+const mekanBilgisi = function(req, res, next) {
+  res.render('mekanbilgisi',
+    {
+      "baslik": 'Mekan Bilgisi',
+      "mekanBaslik": "Starbucks",
+      "mekanDetay": {
+        "ad": "Starbucks",
+        "puan": "5",
+        "adres": "Centrum Garden AVM",
+        "saatler": [
+          {
+            "gunler": "Pazartesi - Cuma",
+            "acilis": "9:00-23:00",
+            "kapali": "false"
+          },
+          {
+            "gunler": "Cumartesi - Pazar",
+            "acilis": "08:00-22:00",
+            "kapali": "false"
+          }
+
+        ],
+        "imkanlar": ["Kahve", "Çay", "Kek"],
+        "koordinatlar": {
+          "enlem": "37.7",
+          "boylam": "30.5"
+        },
+        "yorumlar": [
+          {
+            "yorumYapan": "Kagan Koyuncu",
+            "yorumMetni": "Kahveler Harika",
+            "tarih": "21 Ekim 2022",
+            "puan": "5"
+          },
+          {
+            "yorumYapan": "Ozan Koyuncu",
+            "yorumMetni": "Berbattı",
+            "tarih": "21 Ekim 2022",
+            "puan": "2"
+          }
+        ]
+      }
+    });
 }
 
-const yorumEkle = function (req, res,) {
-  res.render('yorumekle', { 'title': 'Yorum ekle' });
+const yorumEkle = function(req, res, next) {
+  res.render('yorumekle', { title: 'Yorum Ekle' });
 }
 
+
+// anasayfayı dış dünyaya açmak için 
 
 module.exports = {
-
-  anaSayfa,
-  mekanBilgisi,
-  yorumEkle,
-
+  anaSayfa, mekanBilgisi, yorumEkle
 }
